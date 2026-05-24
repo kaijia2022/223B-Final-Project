af21410993785403f66780f9f10ee11714b60c40
kaijia2022 <kaijia2018@outlook.com>
2026-05-24 16:02:35 -0700

    p2p 0.0.0.9

--- Game/src/Game.cpp
+++ Game/src/Game.cpp
@@ -65,7 +65,7 @@ int main() {
         PacketType type = static_cast<PacketType>(msg[offset]);
         if (type == PacketType::CLIENT_INPUT && offset + sizeof(GameStatePacket) <= msg.size()) {
             if (role == NetworkRole::HOST) {
-                ReadyToStartPacket readyConnect;
+                ReadyToStartPacket readyConnect{};
                 std::string outData(reinterpret_cast<char*>(&readyConnect), sizeof(ReadyToStartPacket));
                 bottomLayer.SendNetworkData(outData);
                 printf("Host sent ready to connect packet\n");
